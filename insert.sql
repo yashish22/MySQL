c899d8e92fa2664807eeb3d1c17dcd347c8d661d
yashish22 <yashish22@gmail.com>
2017-05-02 07:33:07 +0530

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `telemedicine1`.`studentinfo` (`idstudentinfo`, `name`, `surname`, `age`) VALUES ('14111001', 'ashish', 'sahu', '20');
+INSERT INTO `telemedicine1`.`studentinfo` (`idstudentinfo`, `name`, `surname`, `age`) VALUES ('14111002', 'ashish', 'yadav', '21');
+INSERT INTO `telemedicine1`.`studentinfo` (`idstudentinfo`, `name`, `surname`, `age`) VALUES ('14111004', 'ayush ', 'khare', '21');
