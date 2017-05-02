ea2212e775902568460b4cd6aad2620fb385baa8
yashish22 <yashish22@gmail.com>
2017-05-02 07:31:58 +0530

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `telemedicine1`.`studentinfo` (
+  `idstudentinfo` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `surname` VARCHAR(45) NULL,
+  `age` INT NULL,
+  PRIMARY KEY (`idstudentinfo`))
+COMMENT = 'biomed 2014-18';
